1d27549ed713876d7b2ab68f0bc92a28a0a2784b
nastyalevk <levkovich.astya@gmail.com>
2021-02-16 17:48:27 +0300

    28 29 30

--- tasks.sql
+++ tasks.sql
@@ -48,26 +48,28 @@ HAVING COUNT(name)>=2
 --14
 SELECT town_to
 FROM (SELECT town_to, P.passenger
-FROM Trip AS T RIGHT JOIN Pass_in_trip AS P ON T.id=P.trip) AS Town_Pass, Passenger
+      FROM Trip AS T RIGHT JOIN Pass_in_trip AS P ON T.id=P.trip) AS Town_Pass, Passenger
 WHERE Town_Pass.passenger=Passenger.id AND
  Passenger.name='Bruce Willis';
 --16
-SELECT name, COUNT(name) as count
+SELECT name, COUNT(name) AS count
 FROM (SELECT T.id, P.passenger
-FROM Trip AS T RIGHT JOIN Pass_in_trip AS P ON T.id=P.trip) AS Trip_Pass, Passenger
+      FROM Trip AS T RIGHT JOIN Pass_in_trip AS P ON T.id=P.trip) AS Trip_Pass, Passenger
 WHERE Trip_Pass.passenger=Passenger.id
 GROUP BY name
 HAVING COUNT(Trip_Pass.id)>=1 
 ORDER BY count DESC ;
 --19
 SELECT status
-FROM (SELECT good_name, family_member FROM Goods RIGHT JOIN Payments ON Goods.good_id=Payments.good) as pg RIGHT JOIN  FamilyMembers ON FamilyMembers.member_id=pg.family_member
+FROM (SELECT good_name, family_member 
+      FROM Goods RIGHT JOIN Payments ON Goods.good_id=Payments.good) AS pg RIGHT JOIN  FamilyMembers ON FamilyMembers.member_id=pg.family_member
 WHERE good_name='potato'
 GROUP BY member_id;
 --20
-SELECT status, member_name, SUM(unit_price*amount) as costs
-FROM (SELECT good_name, Goods.type, family_member, unit_price, amount FROM Goods RIGHT JOIN Payments ON Goods.good_id=Payments.good) as pg RIGHT JOIN  FamilyMembers ON FamilyMembers.member_id=pg.family_member, GoodTypes
-WHERE GoodTypes.good_type_id= pg.type AND  good_type_name='entertainment'
+SELECT status, member_name, SUM(unit_price*amount) AS costs
+FROM (SELECT good_name, Goods.type, family_member, unit_price, amount 
+      FROM Goods RIGHT JOIN Payments ON Goods.good_id=Payments.good) AS pg RIGHT JOIN  FamilyMembers ON FamilyMembers.member_id=pg.family_member, GoodTypes
+      WHERE GoodTypes.good_type_id= pg.type AND  good_type_name='entertainment'
 GROUP BY member_id;
 WHERE good_name='potato'
 GROUP BY member_id;
@@ -86,11 +88,25 @@ SELECT good_name, unit_price
 FROM Payments RIGHT JOIN (SELECT good_id, good_name
                           FROM GoodTypes, Goods
                           WHERE  GoodTypes.good_type_id=Goods.type AND                 
-                          good_type_name='delicacies') as deli ON deli.good_id=Payments.good 
+                          good_type_name='delicacies') AS deli ON deli.good_id=Payments.good 
 WHERE unit_price=(SELECT MAX(unit_price)
                   FROM Payments RIGHT JOIN (
                                 SELECT good_id, good_name
                                 FROM GoodTypes, Goods
-                                WHERE  GoodTypes.good_type_id=Goods.type AND good_type_name='delicacies') as deli 
+                                WHERE  GoodTypes.good_type_id=Goods.type AND good_type_name='delicacies') AS deli 
                                 ON deli.good_id=Payments.good);
- 
+--28
+SELECT COUNT(T.id) AS count
+FROM Trip AS T
+WHERE town_from='Rostov' AND  town_to='Moscow';
+--29
+SELECT name 
+FROM(SELECT passenger
+FROM  Trip LEFT JOIN Pass_in_trip ON Trip.id=Pass_in_trip.trip
+WHERE town_to='Moscow' AND plane='TU-134')as r LEFT JOIN Passenger ON r.passenger=Passenger.id
+GROUP BY  name
+--30
+SELECT trip, COUNT(id) AS count
+FROM Pass_in_trip 
+GROUP BY trip
+ORDER BY count DESC;
